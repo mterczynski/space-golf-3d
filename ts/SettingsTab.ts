790be8f9c5b950e376e01c787bb7ab987b079b72
Michał Terczyński <mterczynski271@gmail.com>
2018-01-25 15:49:27 +0100

    added dat.gui

--- ts/SettingsTab.ts
+++ ts/SettingsTab.ts
@@ -1,8 +1,23 @@
 import { Ball } from "./Ball";
+import { GUI } from "dat-gui";
 
-export abstract class SettingsTab{
-    static updateText(){
-        // document.getElementById('settingsTab__text')!.innerHTML = 
-        // ``;
+export class SettingsTab{
+    constructor(){
+        let gui = new GUI();
     }
+    // var FizzyText = function() {
+    //   this.message = 'dat.gui';
+    //   this.speed = 0.8;
+    //   this.displayOutline = false;
+    //   this.explode = function() { ... };
+    //   // Define render logic ...
+    // };
+
+    // window.onload = function() {
+    //   var text = new FizzyText();
+    //   var gui = new dat.GUI();
+    //   gui.add(text, 'message');
+    //   gui.add(text, 'speed', -5, 5);
+    //   gui.add(text, 'displayOutline');
+    //   gui.add(text, 'explode'); 
 }
