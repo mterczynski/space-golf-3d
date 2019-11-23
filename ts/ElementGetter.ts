365308ab1e071935e8efb22103f1502ade431188
mterczynski <mterczynski271@gmail.com>
2019-11-23 19:09:18 +0100

    code lint

--- ts/ElementGetter.ts
+++ ts/ElementGetter.ts
@@ -1,19 +1,19 @@
-import { Scene, Line } from "three";
-import { Planet } from "./Planet";
+import { Line, Scene } from 'three';
+import { Planet } from './Planet';
 
 export class ElementGetter {
 
 	constructor(private scene: Scene) { }
 
 	getPlanets(): Planet[] {
-		return <Planet[]>this.scene.children.filter((el) => {
+		return this.scene.children.filter((el) => {
 			return el instanceof Planet;
-		});
+		}) as Planet[];
 	}
 
 	getLines(): Line[] {
-		return <Line[]>this.scene.children.filter((el) => {
+		return this.scene.children.filter((el) => {
 			return el instanceof Line;
-		});
+		}) as Line[];
 	}
 }
