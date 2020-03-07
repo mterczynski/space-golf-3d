9fb9b19553b541bfeeb0d06d796e1835efa5133e
Michal Terczynski <mterczynski271@gmail.com>
2020-03-07 19:18:57 +0100

    Aded file loader, added base href

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,27 @@
+const path = require('path');
+
 module.exports = {
-  devtool: 'inline-source-map',
-  entry: './app.ts',
-  output: {
-    filename: 'bundle.js'
-  },
-  resolve: {
-    // Add `.ts` and `.tsx` as a resolvable extension.
-    extensions: ['.ts', '.tsx', '.js'] // note if using webpack 1 you'd also need a '' in the array as well
-  },
-  module: {
-    loaders: [ // loaders will work with webpack 1 or 2; but will be renamed "rules" in future
-      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-      { test: /\.tsx?$/, loader: 'ts-loader' }
-    ]
-  }
+	devtool: 'inline-source-map',
+	entry: './app.ts',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+		publicPath: '/dist'
+	},
+	resolve: {
+		// Add `.ts` and `.tsx` as a resolvable extension.
+		extensions: ['.ts', '.tsx', '.js'] // note if using webpack 1 you'd also need a '' in the array as well
+	},
+	module: {
+		loaders: [ // loaders will work with webpack 1 or 2; but will be renamed "rules" in future
+			// all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+			{ test: /\.tsx?$/, loader: 'ts-loader' },
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: [
+					'file-loader',
+				],
+			},
+		]
+	}
 }
-  
