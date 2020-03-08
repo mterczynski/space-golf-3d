43675adadb450304c0c823edbedc6df70c97c5c3
Michal Terczynski <mterczynski271@gmail.com>
2020-03-08 12:13:30 +0100

    Updated InfoTab

--- ts/InfoTab.ts
+++ ts/InfoTab.ts
@@ -1,22 +1,24 @@
 import { Ball } from './Ball';
 
-export abstract class InfoTab {
-	static updateText(ball: Ball) {
+const htmlElement = document.getElementById('infoTab__text') as HTMLPreElement;
+
+export const InfoTab = {
+	updateText(ball: Ball) {
 		const content =
-			`Ball: {
-	position:{
+`Ball: {
+	position: {
 		x: ${ball.position.x.toFixed(0)},
 		y: ${ball.position.y.toFixed(0)},
 		z: ${ball.position.z.toFixed(0)}
-	}
-	velocity:{
+	},
+	velocity: {
 		length: ${ball.getVelocity().length().toFixed(2)},
 		x: ${ball.getVelocity().x.toFixed(2)},
 		y: ${ball.getVelocity().y.toFixed(2)},
 		z: ${ball.getVelocity().z.toFixed(2)}
 	}
 }`;
-		document.getElementById('infoTab__text')!.innerHTML = content;
 
+		htmlElement.innerHTML = content;
 	}
 }
