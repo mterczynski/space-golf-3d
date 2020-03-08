509a0440e85abf6a234161efce5203f12556c83a
Michal Terczynski <mterczynski271@gmail.com>
2020-03-08 11:28:26 +0100

    Upgraded webpack to v4

--- webpack.config.js
+++ webpack.config.js
@@ -9,12 +9,10 @@ module.exports = {
 		publicPath: '/dist'
 	},
 	resolve: {
-		// Add `.ts` and `.tsx` as a resolvable extension.
-		extensions: ['.ts', '.tsx', '.js'] // note if using webpack 1 you'd also need a '' in the array as well
+		extensions: ['.ts', '.tsx', '.js']
 	},
 	module: {
-		loaders: [ // loaders will work with webpack 1 or 2; but will be renamed "rules" in future
-			// all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+		rules: [
 			{ test: /\.tsx?$/, loader: 'ts-loader' },
 			{
 				test: /\.(png|svg|jpg|gif)$/,
