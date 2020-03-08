c51c64f48a08ad56d7fb577dcbf340dd30256809
Michal Terczynski <mterczynski271@gmail.com>
2020-03-08 09:37:41 +0100

    Updated asset paths to include base href

--- ts/Skybox.ts
+++ ts/Skybox.ts
@@ -1,14 +1,18 @@
 import { BackSide, BoxGeometry, ImageUtils, Mesh, MeshBasicMaterial } from 'three';
 
+function buildSkyboxUrl(part: number) {
+	return `${document.baseURI}/assets/skybox/sky${part}.png`;
+}
+
 export class Skybox extends Mesh {
 	constructor() {
 		const materials = [
-			new MeshBasicMaterial({ side: BackSide, map: ImageUtils.loadTexture('./assets/skybox/sky4.png') }),
-			new MeshBasicMaterial({ side: BackSide, map: ImageUtils.loadTexture('./assets/skybox/sky2.png') }),
-			new MeshBasicMaterial({ side: BackSide, map: ImageUtils.loadTexture('./assets/skybox/sky1.png') }),
-			new MeshBasicMaterial({ side: BackSide, map: ImageUtils.loadTexture('./assets/skybox/sky6.png') }),
-			new MeshBasicMaterial({ side: BackSide, map: ImageUtils.loadTexture('./assets/skybox/sky3.png') }),
-			new MeshBasicMaterial({ side: BackSide, map: ImageUtils.loadTexture('./assets/skybox/sky5.png') }),
+			new MeshBasicMaterial({ side: BackSide, map: ImageUtils.loadTexture(buildSkyboxUrl(4)) }),
+			new MeshBasicMaterial({ side: BackSide, map: ImageUtils.loadTexture(buildSkyboxUrl(2)) }),
+			new MeshBasicMaterial({ side: BackSide, map: ImageUtils.loadTexture(buildSkyboxUrl(1)) }),
+			new MeshBasicMaterial({ side: BackSide, map: ImageUtils.loadTexture(buildSkyboxUrl(6)) }),
+			new MeshBasicMaterial({ side: BackSide, map: ImageUtils.loadTexture(buildSkyboxUrl(3)) }),
+			new MeshBasicMaterial({ side: BackSide, map: ImageUtils.loadTexture(buildSkyboxUrl(5)) }),
 		];
 		const skyboxWidth = 10 ** 5;
 		const geometry = new BoxGeometry(skyboxWidth, skyboxWidth, skyboxWidth);
