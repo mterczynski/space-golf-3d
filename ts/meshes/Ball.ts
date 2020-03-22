5e89c56c2f1835e8967986bd7f43316780dcbd94
Michal Terczynski <mterczynski271@gmail.com>
2020-03-22 10:45:00 +0100

    updated Ball.ts

--- ts/meshes/Ball.ts
+++ ts/meshes/Ball.ts
@@ -23,17 +23,25 @@ function createBallMaterial() {
 export class Ball extends Mesh implements Tickable {
 	private isCollisionBlocked = true;
 	private isOnPlanet = false;
-	private velocity = new Vector3(0, 0, 0);
+	private _velocity = new Vector3(0, 0, 0);
 	private arrowHelper = new ArrowHelper(new Vector3(), new Vector3(), 50);
 	private pathVertices: Vector3[] = [];
 
 	private updateArrowHelper() {
-		this.arrowHelper.setDirection(this.velocity.clone().normalize());
-		this.arrowHelper.setLength(this.velocity.length() * 20);
+		this.arrowHelper.setDirection(this._velocity.clone().normalize());
+		this.arrowHelper.setLength(this._velocity.length() * 20);
 	}
 
 	readonly radius: number;
 	readonly mass = 3;
+
+	get velocity() {
+		return this._velocity.clone();
+	}
+
+	set velocity(newVelocity: Vector3) {
+		this._velocity = newVelocity;
+	}
 
 	constructor({radius = 3}: {
 		radius?: number
@@ -44,7 +52,7 @@ export class Ball extends Mesh implements Tickable {
 	}
 
 	addVelocity(vector: Vector3) {
-		this.velocity.add(vector);
+		this._velocity.add(vector);
 	}
 
 	createTrace() {
@@ -60,44 +68,44 @@ export class Ball extends Mesh implements Tickable {
 	}
 
 	getVelocity() {
-		return this.velocity.clone();
+		return this._velocity.clone();
 	}
 
-	isColliding(planet: Planet) {
-		if (this.isCollisionBlocked) {
-			return false;
-		}
+	// isColliding(planet: Planet) {
+	// 	if (this.isCollisionBlocked) {
+	// 		return false;
+	// 	}
 
-		if (this.position.distanceTo(planet.position) <= this.radius + planet.radius) {
-			// rotate velocity vector by 90 degrees, slow it down
-			const axis = new Vector3(0, -1, 0);
-			const angle = Math.PI / 2;
-			this.velocity.applyAxisAngle(axis, angle).multiplyScalar(0.6);
-			this.isCollisionBlocked = true;
+	// 	if (this.position.distanceTo(planet.position) <= this.radius + planet.radius) {
+	// 		// rotate velocity vector by 90 degrees, slow it down
+	// 		const axis = new Vector3(0, -1, 0);
+	// 		const angle = Math.PI / 2;
+	// 		this.velocity.applyAxisAngle(axis, angle).multiplyScalar(0.6);
+	// 		this.isCollisionBlocked = true;
 
-			if (this.getVelocity().length() <= 0.02) {
-				this.isOnPlanet = true;
-			}
-			setTimeout(() => {
-				this.isCollisionBlocked = false;
-			}, 150);
+	// 		if (this.getVelocity().length() <= 0.02) {
+	// 			this.isOnPlanet = true;
+	// 		}
+	// 		setTimeout(() => {
+	// 			this.isCollisionBlocked = false;
+	// 		}, 150);
 
-			return true;
-		} else {
-			return false;
-		}
-	}
+	// 		return true;
+	// 	} else {
+	// 		return false;
+	// 	}
+	// }
 
-	isCollidingWithAny(planets: Planet[]) {
-		return planets.some(planet => this.isColliding(planet));
-	}
+	// isCollidingWithAny(planets: Planet[]) {
+	// 	return planets.some(planet => this.isColliding(planet));
+	// }
 
 	tick() {
 		if (this.isOnPlanet) {
-			this.velocity = new Vector3();
+			this._velocity = new Vector3();
 		}
 		this.rotation.set(0, 0, 0);
-		this.position.add(this.velocity);
+		this.position.add(this._velocity);
 		const velNorm = this.getVelocity().normalize();
 		this.rotation.set(velNorm.x, velNorm.y, velNorm.z);
 
