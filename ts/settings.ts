0682a4a9cc7d3c29973f85308b4d46eef1723653
Michał Terczyński <mterczynski271@gmail.com>
2022-04-09 19:35:36 +0200

    Sort settings

--- ts/settings.ts
+++ ts/settings.ts
@@ -1,9 +1,9 @@
 export const settings = Object.freeze({
 	ball: Object.freeze({
+		bounciness: 0.8,
 		launchForce: 6,
 		showVelocityVector: false,
 		traceDuration: 0,
-		bounciness: 0.8,
 	}),
 	defaultPlanetDensity: 0.00014,
 	showFPSCounter: true,
