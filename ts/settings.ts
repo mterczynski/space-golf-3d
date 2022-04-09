bb32994d53c2d57f6fe0f82131fd41144ecf51b7
Michał Terczyński <mterczynski271@gmail.com>
2022-04-09 19:44:27 +0200

    Change traceDuration to 1

--- ts/settings.ts
+++ ts/settings.ts
@@ -3,7 +3,7 @@ export const settings = Object.freeze({
 		bounciness: 0.8,
 		launchForce: 7,
 		showVelocityVector: false,
-		traceDuration: 5,
+		traceDuration: 1,
 		traceTransparency: 0.1
 	}),
 	defaultPlanetDensity: 0.00014,
