d2c05c745903fa9062b333ade8e97c4f383d1258
Michał Terczyński <mterczynski1@gmail.com>
2024-02-05 21:20:16 +0100

    Create getInstancesOf method

--- src/ElementGetter.ts
+++ src/ElementGetter.ts
@@ -1,14 +1,18 @@
-import { Line, Scene } from 'three';
+import { Line, Object3D, Scene } from 'three';
 import { Planet } from './meshes/Planet';
 
 export class ElementGetter {
 	constructor(private scene: Scene) { }
 
 	getPlanets(): Planet[] {
-		return this.scene.children.filter(el => el instanceof Planet) as Planet[];
+		return this.getInstancesOf(Planet)
 	}
 
 	getLines(): Line[] {
-		return this.scene.children.filter(el => el instanceof Line) as Line[];
+		return this.getInstancesOf(Line)
+	}
+
+	private getInstancesOf<T extends Object3D>(klass: { new(...args: any[]): T }): T[] {
+		return this.scene.children.filter(el => el instanceof klass) as T[];
 	}
 }
