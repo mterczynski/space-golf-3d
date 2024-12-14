8fc8b6cd297abb0770df7af76a8fbc89d63d208c
Chuks <63956312+skido600@users.noreply.github.com>
2024-12-14 14:55:48 +0100

    removing tslint and adding eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,12 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-	{ files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-	{ files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-	{ languageOptions: { globals: globals.browser } },
-	pluginJs.configs.recommended,
-	...tseslint.configs.recommended,
-	{
-		...pluginReact.configs.flat.recommended,
-		settings: {
-			react: {
-				version: "detect",
-			},
-		},
-	},
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
 ];
