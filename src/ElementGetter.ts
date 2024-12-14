045486122fe9acc7ecf6bdf327c38e6febb15915
Chuks <63956312+skido600@users.noreply.github.com>
2024-12-14 16:50:31 +0100

    removing tslint and adding eslint v2

--- src/ElementGetter.ts
+++ src/ElementGetter.ts
@@ -1,18 +1,20 @@
-import { Line, Object3D, Scene } from 'three';
-import { Planet } from './meshes/Planet';
+import { Line, Object3D, Scene } from "three";
+import { Planet } from "./meshes/Planet";
 
 export class ElementGetter {
-	constructor(private scene: Scene) { }
+	constructor(private scene: Scene) {}
 
 	getPlanets(): Planet[] {
-		return this.getInstancesOf(Planet)
+		return this.getInstancesOf(Planet);
 	}
 
 	getLines(): Line[] {
-		return this.getInstancesOf(Line)
+		return this.getInstancesOf(Line);
 	}
 
-	private getInstancesOf<T extends Object3D>(klass: { new(...args: any[]): T }): T[] {
-		return this.scene.children.filter(el => el instanceof klass) as T[];
+	private getInstancesOf<T extends Object3D>(klass: {
+		new (...args: any[]): T;
+	}): T[] {
+		return this.scene.children.filter((el) => el instanceof klass) as T[];
 	}
 }
